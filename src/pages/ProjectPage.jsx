810e0a32796dab5a90e0bf8574e340ebfaf7a216
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-05-14 16:52:37 +0800

    projects can now received pledges - only first pledge showing

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -5,6 +5,10 @@ import { useParams } from "react-router-dom";
 import UserDetail from "../components/ProjectOwner/ProjectOwner";
 import PledgeForm from "../components/PledgeForm/PledgeForm";
 
+//styles
+import "./ProjectSection.css";
+
+
 function ProjectPage() {
     const [projectData, setProjectData] = useState({pledges: [] });
     const { id } = useParams();
@@ -30,29 +34,39 @@ function ProjectPage() {
     return (
         // instead of <div> or you can use <> instead of React.Fragment 
         <>
-        <div>
-            <h2>{projectData.title}</h2>
-            <h3>Created by: <UserDetail userId={projectData.owner} /> on {projectData.date_created}</h3>
-        </div>
+        <div className="project-container">
+            <section className="project-title">
+                <h2>{projectData.title}</h2>
+                {/* <h3>Created by: <UserDetail userId={projectData.owner} /> </h3> */}
+                {/* on {projectData.date_created}</h3> */}
+            </section>
 
-        <div>
-        <h2>{projectData.title}</h2>
-            <h2>Time left: {projectData.time} hours</h2>
-            {/* <h2>//do an array with pledge time - project time</h2> */}
-            <h3>Created at: {projectData.date_created}</h3>
-            <h3>{`Status: ${projectData.is_open}`}</h3>
-            <h3>Pledges:</h3>
-            <ul>
-            {projectData.pledges.map((pledgeData, key) => {
-                return (
-                <li>
-                    {pledgeData.amount} from {pledgeData.supporter}
-                </li>
-                );
-            })}
-            </ul>
-            <PledgeForm projectId={id}/>
-        </div>          
+            <container className="project-content">
+            {/* <h2>{projectData.title}</h2> */}
+            <h1><UserDetail userId={projectData.owner} /></h1> <h2>Is asking for {projectData.time} hours of your time</h2>
+            <h2>{projectData.comment}</h2>  
+                {/* <h2>//do an array with pledge time - project time</h2> */}
+                {/* <h3>Created at: {projectData.date_created}</h3> */}
+                {/* <h3>{`Status: ${projectData.is_open}`}</h3> */}
+                <h3>Pledges:</h3>
+                <container className="pledges">
+                <ul>
+                {projectData.pledges.map((pledgeData, key) => {
+                    return (
+                        
+                            <li className="pledge-list">
+                                {pledgeData.amount} hours donated from {pledgeData.supporter} {pledgeData.comment}
+                             </li>
+                    
+                    );
+                 })}
+                </ul>
+                </container>
+               
+                <PledgeForm projectId={id}/>
+            </container>    
+        </div>
+          
         </>
     );
 }
